a53c95b80de303795e8310bd3576ac97ffdbafd3
amira bennani <amirabennani1955m@gmail.com>
2020-09-20 18:38:25 +0100

    Update page state matcher.

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
+let flatten = (children, getChildren, level, parent) => Array.prototype.concat.apply(
+    children.map(x => ({ ...x, level: level || 1, parent: parent || null })), 
+    children.map(x => flatten(getChildren(x) || [], getChildren, (level || 1) + 1, x.id))
+  );
+
+let extractChildren = x => x.children;
+
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log("The color is green.");
@@ -5,7 +12,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
+            pageUrl: {hostContains: ''},
           })
           ],
               actions: [new chrome.declarativeContent.ShowPageAction()]
