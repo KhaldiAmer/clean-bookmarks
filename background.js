5f76e0c2efe3a2780ee840330cab6c312a8f1bc3
amira bennani <amirabennani1955m@gmail.com>
2020-09-20 19:39:31 +0100

    Remove storage func.

--- background.js
+++ background.js
@@ -6,9 +6,6 @@ let flatten = (children, getChildren, level, parent) => Array.prototype.concat.a
 let extractChildren = x => x.children;
 
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
