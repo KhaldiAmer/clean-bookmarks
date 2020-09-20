5ac3c9986ec98a22cb8c4d291302b297da528391
amira bennani <amirabennani1955m@gmail.com>
2020-09-20 19:39:54 +0100

    Add code to popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,26 @@
-let changeColor = document.getElementById('changeColor');
+let flatten = (children, getChildren, level, parent) => Array.prototype.concat.apply(
+    children.map(x => ({ ...x, level: level || 1, parent: parent || null })), 
+    children.map(x => flatten(getChildren(x) || [], getChildren, (level || 1) + 1, x.id))
+  );
 
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
+let extractChildren = x => x.children;
+
+
+    chrome.bookmarks.getTree (([treeStructure]) => {
+        console.log(treeStructure)
+        let arr = flatten(extractChildren(treeStructure), extractChildren).map(x => delete x.children && x);
+        console.log("arr ", arr);
+        let item = document.getElementById("totalBookmarks");
+        item.innerHTML = `Total bookmarks: ${arr.length} `
+    })
+
+// chrome.storage.sync.get('color', function (data) {
+//     changeColor.style.backgroundColor = data.color;
+//     changeColor.setAttribute('value', data.color);
+// });
+// chrome.storage.sync.set({color: '#3aa757'}, function() {
+//     console.log("The color is green.");
+//   });
 
 changeColor.onclick = function (element) {
     let color = element.target.value;
